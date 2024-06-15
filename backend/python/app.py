d92c0dbec9486ef5a9d0e5da34563ec5a0c56816
surabhiwaingankar <surabhiwaingankar@gmail.com>
2024-06-15 22:53:08 +0530

    backend: emails function added and integrated

--- backend/python/app.py
+++ backend/python/app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 import requests
 import speech_comparison as sr
+import emails as email
 
 app = Flask(__name__)
 CORS(app) 
@@ -13,6 +14,14 @@ def product_info():
     user_text =data['user_text']
     output = sr.get_answer(original_text, user_text)
     return jsonify({"answer": output})
+
+@app.route('/email', methods=['POST'])
+def send_mail():
+    data = request.json
+    content = data['content']
+    recipients = data['recipients']
+    email.send_email(content, recipients)
+    return jsonify({"output": "Email sent!"})
 
 if __name__ == '__main__':
     app.run(debug=True)
